2037a4d9ca1d8d0fd2431bbe82b67fe599c27c60
Evan Susag <emseduau@gmail.com>
2018-06-15 08:57:12 -0500

    here u go

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,24 @@
-const chooseRandom = (array, numItems) => {
-  // TODO check that array length is greater than 1
-  const randomIndices = Array(numItems) // TODO ensure numItems is within the range 1 to the length of the array
-    .fill() // Don't change this line. Just fills the array we're about to reduce.
-    .reduce((res, next) => {
-      // reduce the array to contain numItems # of unique index values
-    })
-  // TODO return a new array filtering the passed in array for only the
-  // indices contained in the randomIndices array
+const chooseRandom = (array = [], numItems = 1) => {
+  return array.length <= 1 || numItems < 1
+    ? array
+    : Array(
+        numItems > array.length || numItems < 0
+          ? Math.floor(Math.random() * array.length)
+          : numItems
+      )
+        .fill()
+        .reduce(
+          res =>
+            res.concat(
+              Array.apply(null, { length: array.length })
+                .map(Number.call, Number)
+                .filter(vally => res.indexOf(vally) < 0)[
+                Math.floor(Math.random() * (array.length - res.length) * 2) %
+                  (array.length - res.length)
+              ]
+            ),
+          []
+        )
+        .map(currVal => array[currVal])
 }
+export { chooseRandom }
--- src/test.js
+++ src/test.js
@@ -4,12 +4,12 @@ import { chooseRandom } from './lib'
 
 describe('chooseRandom', () => {
   it('Should return an array', () => {
-    jsv.assertForall('array nat', (arr) => {
+    jsv.assertForall('array nat', arr => {
       return Array.isArray(chooseRandom(arr))
     })
   })
   it('Should not mutate the array', () => {
-    jsv.assertForall('array nat', (arr) => {
+    jsv.assertForall('array nat', arr => {
       const arrBefore = arr
       chooseRandom(arr)
       return arrBefore === arr
@@ -21,14 +21,16 @@ describe('chooseRandom', () => {
     })
   })
   it('Should return an array with length equal to the given number (if provided)', () => {
-    jsv.assertForall('array nat', (arr) => {
+    jsv.assertForall('array nat', arr => {
       if (arr.length === 0 || arr.length === 1) {
         // Here the numItems value given to chooseRandom is irrelevent as
         // an array of length 0 or 1 should just return the given array
         // with no possibility of randomization.
-        return arr === chooseRandom(arr, 948672894968) &&
+        return (
+          arr === chooseRandom(arr, 948672894968) &&
           arr === chooseRandom(arr, 0) &&
           arr === chooseRandom(arr)
+        )
       }
       const random = chooseRandom(arr, 2)
       return random.length === 2
